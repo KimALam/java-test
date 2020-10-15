9c19b621a4e15ed8b3aa7f8a237038dca53fe256
Kim A Lam <alkim78@gmail.com>
2020-10-15 18:17:15 +0900

    Signature test

---
+++ src/main/java/security/signature/Receive.java
@@ -0,0 +1,65 @@
+package security.signature;
+
+import lombok.extern.slf4j.Slf4j;
+import security.cert.KeyStoreHandler;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.security.KeyStore;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.cert.Certificate;
+
+@Slf4j
+class Receive {
+    private static final String ALIAS = "sdo";
+    private static final String PW = "1234qwer";
+    private static final String FILE_NAME = "sig_test";
+
+    public static void main(String[] args) {
+        try {
+            String data = null;
+            byte[] signature = null;
+
+            FileInputStream fis = new FileInputStream(FILE_NAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            // read plain data
+            Object o = ois.readObject();
+            try {
+                data = (String) o;
+            } catch (ClassCastException cce) {
+                log.error("Unexpected data in file");
+                System.exit(-1);
+            }
+
+            // read signature
+            o = ois.readObject();
+            try {
+                signature = (byte[]) o;
+            } catch (ClassCastException cce) {
+                log.error("Unexpected data in file");
+                System.exit(-1);
+            }
+
+            log.info("Received message :{}", data);
+
+            KeyStoreHandler ksh = new KeyStoreHandler(null);
+            KeyStore ks = ksh.getKeyStore();
+
+            // certificate has public key + signature + info(ex. DN)
+            Certificate cert = ks.getCertificate(ALIAS);
+            PublicKey pk = cert.getPublicKey();
+            Signature s = Signature.getInstance("MD5withRSA");
+            s.initVerify(pk);
+            s.update(data.getBytes());
+            if (s.verify(signature)) {
+                log.info("Message is valid");
+            } else {
+                log.info("Message was corrupted");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/security/signature/Send.java
+++ src/main/java/security/signature/Send.java
@@ -1,22 +1,39 @@
 package security.signature;
 
+import security.cert.KeyStoreHandler;
+
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Signature;
 
 class Send {
+    private static final String ALIAS = "sdo";
+    private static final String PW = "1234qwer";
     private static final String FILE_NAME = "sig_test";
+    private static final String data = "This have I thought good to deliver thee, " +
+            "that thou mightst not lose the dues of rejoicing " +
+            "by being ignorant of what greatness is promised thee.";
 
     public static void main(String[] args) {
-        String data = "This have I thought good to deliver thee, " +
-                      "that thou mightst not lose the dues of rejoicing " +
-                      "by being ignorant of what greatness is promised thee.";
-
         try {
             FileOutputStream fos = new FileOutputStream(FILE_NAME);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            // TODO
-        } catch (IOException e) {
+
+            KeyStoreHandler ksh = new KeyStoreHandler(null);
+            KeyStore ks = ksh.getKeyStore();
+            PrivateKey pk = (PrivateKey) ks.getKey(ALIAS, PW.toCharArray());
+
+            Signature s = Signature.getInstance("MD5withRSA");
+            s.initSign(pk);
+
+            byte[] buf = data.getBytes();
+            s.update(buf);
+
+            oos.writeObject(data);
+            oos.writeObject(s.sign());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
